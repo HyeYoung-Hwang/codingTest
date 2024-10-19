b3acab9cb65786947340ef3b142b503f91433798
HyeYoung-Hwang <114129207+HyeYoung-Hwang@users.noreply.github.com>
2024-10-19 21:26:45 +0900

    [level 2] Title: 올바른 괄호, Time: 20.44 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,27 +2,24 @@ import java.util.Stack;
 
 class Solution {
     boolean solution(String s) {
-        try{
-            boolean answer = false;
         
-        Stack st = new Stack();
+        boolean answer = true;
+        
+        Stack<Character> st = new Stack();
         
         for(int i=0; i<s.length(); i++){
-            if(s.charAt(0) == ')'){
-                break;
-            }
             if(s.charAt(i) == '('){
                 st.push(s.charAt(i));
             }else if(s.charAt(i) == ')'){
-                st.pop();
+                if(st.empty()){
+                    return false;
+                }else{
+                    st.pop();
+                }
             }
+        }
         answer = st.empty();
-        }
         return answer;
-        }
-        catch(Exception e){
-            return false;
-        }
         
     }
 }
