0cb2598aa901d851a0c7a852d1384184fd2e6a67
HyeYoung-Hwang <114129207+HyeYoung-Hwang@users.noreply.github.com>
2025-07-09 12:57:48 +0900

    [level 1] Title: 모의고사, Time: 3.85 ms, Memory: 92.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] answers) {
+        int[] a = {1,2,3,4,5};
+        int[] b = {2,1,2,3,2,4,2,5};
+        int[] c = {3,3,1,1,2,2,4,4,5,5};
+        int scores[] = new int[3];
+        
+        for(int i=0; i<answers.length; i++){
+            if(answers[i] == a[i%a.length]){
+                scores[0]++;
+            }
+            if(answers[i] == b[i%b.length]){
+                scores[1]++;
+            }
+            if(answers[i] == c[i%c.length]){
+                scores[2]++;
+            }
+        }
+        
+        int maxScore = Math.max(scores[0], Math.max(scores[1], scores[2]));
+        
+        ArrayList<Integer> result = new ArrayList<>();
+        for(int i=0; i<scores.length; i++){
+            if(scores[i] == maxScore){
+                result.add(i+1);
+            }
+        }
+        
+        return result.stream().mapToInt(i->i).toArray();
+    }
+}
